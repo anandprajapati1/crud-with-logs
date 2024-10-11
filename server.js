c1b0b6f7799d0e3877dbbb1b3bae17fe07cce08e
Anand Prajapati <anandprajapati1@users.noreply.github.com>
2024-10-11 22:17:22 +0530

    implement cors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // server.js
 const express = require('express');
 const app = express();
+// implement cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Your API routes
 app.get('/api', (req, res) => {

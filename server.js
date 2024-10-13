6a12b8e904a4f7529b7604e4f6339955540aa990
Anand Prajapati <anandprajapati1@users.noreply.github.com>
2024-10-13 12:29:09 +0530

    post data in req body

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.get('/api', (req, res) => {
     res.json({ message: 'Hello from Express deployed on Vercel!' });
 });
 
+app.post('/post-data', (req, res) => {
+    const { fullname, mobile, lat, lng } = req.body;
+    console.log(`Name: ${fullname}\nMobile: ${mobile}\nLocation: ${lat}, ${lng}`);
+    
+    res.send({
+        "success": true
+    })
+})
+
 // Listen on port 8080 if run locally
 app.listen(8080, () => {
     console.log('Server running on http://localhost:8080');
